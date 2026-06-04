b01ca16be0a5186956ba0798a3f85922368afa6b
EstalillaSenpai <joaquinestalilla1223@gmail.com>
2026-06-05 03:19:38 +0800

    Hosting updates

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import server from "../src/server";
+import server from "../src/server.ts";
 
 function readRawBody(req: IncomingMessage): Promise<Buffer> {
   return new Promise((resolve, reject) => {
